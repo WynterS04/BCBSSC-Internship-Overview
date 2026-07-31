5a571705b3c3b6efc3813ee73bcfb50b92584b7a
Wynter Stroman <Wynter.Stroman@bcbssc.com>
2026-07-31 16:43:27 -0400

    add content cards

---
+++ docs/components/ContentCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps[{
+    icon:{required: true},
+    title:{required: true}
+}]
+
+</script>
+<template>
+<div class="card-container">
+    <div class="icon">{{ props.icon }}</div>
+    <div class="title">{{ props.title }}</div>
+</div>
+</template>
+<style>
+
+</style>
